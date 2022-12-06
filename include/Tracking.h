ce81338265e38c8b1a318534b0075b41389d1d89
Larissa A. Santesso <larissasantesso@gmail.com>
2022-12-06 14:53:37 -0300

    Update Tracking.h

--- include/Tracking.h
+++ include/Tracking.h
@@ -59,6 +59,7 @@ public:
 
     // Preprocess the input and call Track(). Extract features and performs stereo matching.
     cv::Mat GrabImageStereo(const cv::Mat &imRectLeft,const cv::Mat &imRectRight, const double &timestamp);
+    cv::Mat GrabImageStereoWithMask((const cv::Mat &imRectLeft,const cv::Mat &imRectRight, const cv::Mat &maskRectLeft, const cv::Mat &maskRectRight, const double &timestamp);
     cv::Mat GrabImageRGBD(const cv::Mat &imRGB,const cv::Mat &imD, const double &timestamp);
     cv::Mat GrabImageMonocular(const cv::Mat &im, const double &timestamp);
 
