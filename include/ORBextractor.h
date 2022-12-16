89db9b79e6cfadabae42c8b202e01f3465915a97
Larissa A. Santesso <larissasantesso@gmail.com>
2022-12-16 15:43:19 -0300

    Update ORBextractor.h

--- include/ORBextractor.h
+++ include/ORBextractor.h
@@ -90,13 +90,13 @@ protected:
     void ComputePyramid(cv::Mat image);
     void ComputePyramidWithMask(cv::Mat image, cv::Mat mask);
     
-    void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);    
-    std::vector<cv::KeyPoint> DistributeOctTree(const std::vector<cv::KeyPoint>& vToDistributeKeys, const int &minX,
-                                           const int &maxX, const int &minY, const int &maxY, const int &nFeatures, const int &level);
+    void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint> >& allKeypoints); 
     
-    void ComputeKeyPointsOctTreeWithMask(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);    
+    void ComputeKeyPointsOctTreeWithMask(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);  
+    
     std::vector<cv::KeyPoint> DistributeOctTree(const std::vector<cv::KeyPoint>& vToDistributeKeys, const int &minX,
-                                           const int &maxX, const int &minY, const int &maxY, const int &nFeatures, const int &level);
+                                           const int &maxX, const int &minY, const int &maxY, const int &nFeatures, const int &level);  
+    
 
     void ComputeKeyPointsOld(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);
     std::vector<cv::Point> pattern;
