486a4bfc6fe393caf5aeb820c3367a07412ee22b
Larissa A. Santesso <larissasantesso@gmail.com>
2023-02-07 15:18:32 -0300

    Update stereo_cityscapes.cc

--- Examples/Stereo/stereo_cityscapes.cc
+++ Examples/Stereo/stereo_cityscapes.cc
@@ -171,8 +171,8 @@ void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
         }
     }
 
-    string strPrefixLeft = strPathToSequence + "/image_0_resized/";
-    string strPrefixRight = strPathToSequence + "/image_1_resized/";
+    string strPrefixLeft = strPathToSequence + "/image_resized_0/";
+    string strPrefixRight = strPathToSequence + "/image_resized_1/";
 
     const int nTimes = vTimestamps.size();
     vstrImageLeft.resize(nTimes);
